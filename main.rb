45f312425d7960214c6aded3b922b4d587cf9402
EC2 Default User <ec2-user@ip-172-31-54-129.ec2.internal>
2022-02-06 10:25:27 +0000

    ジャンケンアプリ課題提出

--- main.rb
+++ main.rb
@@ -1,82 +1,413 @@
+loop do
 
-# require "pry"
 puts "ジャンケン"
 puts "0(グー)1(チョキ)2(パー)3(戦わない)"
-select_hand = gets.to_i
+select_number = gets.to_i
 
-# 変数定義まとめる
 hand_gestures = ["グー", "チョキ", "パー"]
+rival_gesture = hand_gestures.sample
 hand_directions = ["上", "下", "左", "右"]
-rival_gesture = hand_gestures.sample(1).to_s
-hand_direction_up = hand_directions[0]
-rival_direction = hand_directions.sample(1).to_s
+rival_direction = hand_directions.sample
 
-# グーを出した時
+case select_number
+when 0
+select_hand = hand_gestures[0]
+when 1
+select_hand = hand_gestures[1]
+when 2
+select_hand = hand_gestures[2]
+else
+puts "終了"
+break
+end
 
-if select_hand == 0
-puts "ホイ"
-puts "-------------------"
-puts "あなた：" + hand_gestures[0] + "を出しました"
-puts "相手：#{rival_gesture}を出しました"
-
-puts rival_gesture.class
-puts hand_directions[0].class
-
-# binding.pry
-    if rival_gesture == hand_gestures[0]
-      puts "ジャンケンの繰り返し処理行う"
-    elsif rival_gesture == hand_gestures[1]
-      puts "指をさす方向を決める処理行う"
-    elsif rival_gesture == hand_gestures[2]
-      puts "顔を向ける方向を決める処理行う"
-    else
-      puts "失敗"
+  case select_hand    
+  when "グー"
+    puts "ホイ"
+    puts "-------------------"
+    puts "あなた：" + select_hand + "を出しました"
+    puts "相手：#{rival_gesture}を出しました"
+    puts "-------------------"
+   
+    case rival_gesture
+    when "グー"
+      puts "あいこです"
+     
+    when "チョキ"
+      puts "こちらが勝っています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[0]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[1]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[2]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[3]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        end  
+    
+    when "パー"
+      puts "こちらが負けています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[0]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[1]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[2]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[3]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        end 
     end
-puts "-------------------"
-
-
-puts "あっち向いてー"
-puts "0(上)1(下)2(左)3(右)"
-select_direction = gets.to_i
-
-
-    if select_direction == 0
+  
+  when "チョキ"
+    rival_gesture = hand_gestures.sample
+    puts "ホイ"
+    puts "-------------------"
+    puts "あなた：" + select_hand + "を出しました"
+    puts "相手：#{rival_gesture}を出しました"
+    puts "-------------------"
     
-    puts "あなた：" + hand_direction_up + "を出しました"
-    puts "相手：#{rival_direction}を出しました"
+    case rival_gesture
+    when "グー"
+     puts "こちらが負けています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[0]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[1]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[2]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[3]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        end 
+    
+    when "チョキ"
+     puts "あいこです"
+     
+    when "パー" 
+     puts "こちらが勝っています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[0]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[1]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[2]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[3]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        end 
+    end  
     
     
-        if rival_direction == hand_direction_up
-        puts "終了"
+  when "パー"
+    rival_gesture = hand_gestures.sample
+    puts "ホイ"
+    puts "-------------------"
+    puts "あなた：" + select_hand + "を出しました"
+    puts "相手：#{rival_gesture}を出しました"
+    puts "-------------------"
+    
+    case rival_gesture
+    when "グー"
+     puts "こちらが勝っています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[0]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[1]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[2]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を指しました"
+        puts "相手：#{rival_direction}を向きました"
+          case rival_direction
+          when hand_directions[3]
+              puts "-------------------"
+              puts "終了。あなたの勝ち"
+              break
+          else
+              puts "もう一度" 
+              puts "-------------------"
+          end 
+        end  
         
-        elsif rival_direction != hand_direction_up
-        puts "もう一度"    
-        
-        end
-    　
-    elsif select_direction == 1
-    puts "1"   
-    　
-    elsif select_direction == 2
-    puts "2"
-    　
-    elsif select_direction == 3
-    puts "3"
-    　
+    when "チョキ"
+     puts "こちらが負けています"
+      puts "あっち向いてー"
+      puts "0(上)1(下)2(左)3(右)"
+      choice_number = gets.to_i
+      puts "ホイ"
+      puts "-------------------"
+        case choice_number
+        when 0
+        puts "あなた：" + hand_directions[0] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[0]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end    
+        when 1
+        puts "あなた：" + hand_directions[1] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[1]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 2
+        puts "あなた：" + hand_directions[2] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[2]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        when 3
+        puts "あなた：" + hand_directions[3] + "を向きました"
+        puts "相手：#{rival_direction}を指しました"
+          case rival_direction
+          when hand_directions[3]
+            puts "-------------------"
+            puts "終了。あなたの負け"
+            break
+          else
+            puts "もう一度" 
+            puts "-------------------"
+          end 
+        end 
+    when "パー"  
+     puts "あいこです"
     end
-    
-
-elsif select_hand == 1
-    
-puts "チョキ選択"
-
-elsif select_hand == 2
-    
-puts "パー選択"
-
-elsif select_hand == 3
-    
-puts "最初からやり直してください" 
-exit
-
+  else
+    puts "もう一度"
+  end
 end
